fbd2c8b20e4da53ea1c620ca5e66cb4bd56a3d64
Kurt-Erik Karlsen <530617@student.hin.no>
2015-04-14 00:26:44 +0200

    Added map line between cities and pin

--- app/src/main/java/no/hin/student/mymapapp/MainActivity.java
+++ app/src/main/java/no/hin/student/mymapapp/MainActivity.java
@@ -1,28 +1,45 @@
 package no.hin.student.mymapapp;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 
 public class MainActivity extends Activity
 {
     GoogleMap googleMap;
+    String TAG = "Prosjekt: ";
+    private static final LatLng STOKMARKNES = new LatLng(68.568854, 14.945730);
+    private static final LatLng INNHAVET = new LatLng(67.968125 , 15.926599);
+    private static final LatLng NARVIK = new LatLng(68.434521,  17.420488);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         googleMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
         googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-        googleMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Test").draggable(true));
+        if (googleMap != null) {
+            addLines();
+        }
+    }
+
+    private void addLines() {
+        googleMap.addMarker(new MarkerOptions().position(STOKMARKNES).title("Stokmarknes").draggable(true));
+        googleMap.addMarker(new MarkerOptions().position(INNHAVET).title("Innhavet").draggable(true));
+        googleMap.addMarker(new MarkerOptions().position(NARVIK).title("Narvik").draggable(true));
+        googleMap.addPolyline((new PolylineOptions()).add(INNHAVET, NARVIK, STOKMARKNES, INNHAVET).width(5).color(Color.CYAN).geodesic(true));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(STOKMARKNES, 13));
     }
 
 
